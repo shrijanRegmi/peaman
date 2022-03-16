16714e08a4e9c1832426a5ce90fb0026fd7dcb86
Shrijan Regmi <ilyyhs9@gmail.com>
2022-03-16 17:30:19 +0545

    removed addDataToDatabase, getDatasFromDatabase and getSingleDataFromDatabase

--- lib/main/peaman_initializer.dart
+++ lib/main/peaman_initializer.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:peaman/peaman.dart';
 
 class Peaman {
   static final _ref = FirebaseFirestore.instance;
@@ -9,37 +8,5 @@ class Peaman {
     return await Firebase.initializeApp();
   }
 
-  static Future<void> addDataToDatabase({
-    required final String docPath,
-    required final Map<String, dynamic> data,
-  }) async {
-    try {
-      final _reff = _ref.doc(docPath);
-      await _reff.set(data);
-      print('Success: Adding data to database $docPath');
-    } catch (e) {
-      print(e);
-      print('Error!!!: Adding data to database');
-    }
-  }
-
-  static Stream<List<dynamic>> getDatasFromDatabase({
-    required final String colPath,
-    final PeamanQuery? query,
-  }) {
-    return _ref
-        .collection(colPath)
-        .where('${query?.whereKey}', isEqualTo: query?.whereValue)
-        .orderBy('${query?.orderBy}', descending: query?.descending ?? false)
-        .snapshots()
-        .map(
-          (event) => event.docs.map((e) => e.data()).toList(),
-        );
-  }
-
-  static Stream<dynamic> getSingleDataFromDatabase({
-    required final String docPath,
-  }) {
-    return _ref.doc(docPath).snapshots().map((event) => event.data());
-  }
+  static FirebaseFirestore get ref => _ref;
 }
