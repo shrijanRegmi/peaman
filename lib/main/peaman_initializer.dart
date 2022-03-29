5a96d66a90f0a5acd985f19bd078c825639a4fdf
Shrijan Regmi <ilyyhs9@gmail.com>
2022-03-29 19:03:57 +0545

    getting currentUid implemented

--- lib/main/peaman_initializer.dart
+++ lib/main/peaman_initializer.dart
@@ -1,12 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class Peaman {
   static final _ref = FirebaseFirestore.instance;
+  static final _auth = FirebaseAuth.instance;
+  static final _uid = _auth.currentUser?.uid;
 
-  static Future initializeApp() async {
-    return await Firebase.initializeApp();
+  static Future<FirebaseApp> initializeApp() {
+    return Firebase.initializeApp();
   }
 
   static FirebaseFirestore get ref => _ref;
+  static String? get currentUid => _uid;
 }
