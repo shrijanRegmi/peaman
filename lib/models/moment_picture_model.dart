95cb4873ce14178f34eb648c8c1177e39b85ef83
Shrijan Regmi <ilyyhs9@gmail.com>
2022-03-30 14:22:40 +0545

    moment feature improved

--- lib/models/moment_picture_model.dart
+++ lib/models/moment_picture_model.dart
@@ -2,11 +2,13 @@ class PeamanMomentPicture {
   final String? id;
   final String url;
   final int updatedAt;
+  final int expiresAt;
 
   PeamanMomentPicture({
     this.id,
     this.url = '',
     this.updatedAt = -1,
+    this.expiresAt = -1,
   });
 
   static PeamanMomentPicture fromJson(final Map<String, dynamic> data) {
@@ -14,6 +16,7 @@ class PeamanMomentPicture {
       id: data['id'],
       url: data['url'] ?? '',
       updatedAt: data['updated_at'] ?? -1,
+      expiresAt: data['expires_at'] ?? -1,
     );
   }
 
@@ -22,6 +25,7 @@ class PeamanMomentPicture {
       'id': id,
       'url': url,
       'updated_at': updatedAt,
+      'expires_at': expiresAt,
     };
   }
 }
