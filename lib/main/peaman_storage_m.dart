b284a9b77ac02f9568b2dd28e87fc9cb5c3c48c5
Shrijan Regmi <ilyyhs9@gmail.com>
2022-09-04 10:03:09 +0545

    storage provider arguments updated

--- lib/main/peaman_storage_m.dart
+++ lib/main/peaman_storage_m.dart
@@ -3,9 +3,9 @@ import 'package:peaman/services/storage_services/storage_provider.dart';
 
 class PStorageProvider {
   // upload file
-  static Future<String?> uploadFile(
-    final String path,
-    final File file, {
+  static Future<String?> uploadFile({
+    required final String path,
+    required final File file,
     final Function(double)? onProgressUpdate,
   }) async {
     return await StorageProvider(
@@ -16,9 +16,9 @@ class PStorageProvider {
   }
 
   // upload files
-  static Future<List<String>?> uploadFiles(
-    final String path,
-    final List<File> files, {
+  static Future<List<String>?> uploadFiles({
+    required final String path,
+    required final List<File> files,
     final Function(double)? onProgressUpdate,
   }) async {
     return await StorageProvider(
