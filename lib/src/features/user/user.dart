eece80db700ca623d9bb67190592777171903b58
Shrijan Regmi <developer.shrijan@gmail.com>
2023-03-17 14:55:36 +0545

    fixed export of peaman user repo

--- lib/src/features/user/user.dart
+++ lib/src/features/user/user.dart
@@ -5,3 +5,5 @@ export 'models/follow_request_model.dart';
 export 'models/following_model.dart';
 export 'models/sub_user_model.dart';
 export 'models/user_model.dart';
+
+export 'repositories/user_repository.dart';
