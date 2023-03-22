5e53fcc16637a66b9bc1595bb2c465e39d9d6e09
Shrijan Regmi <developer.shrijan@gmail.com>
2023-03-22 10:02:33 +0545

    added string value of gender

--- lib/src/features/user/enums/gender_type.dart
+++ lib/src/features/user/enums/gender_type.dart
@@ -4,3 +4,10 @@ enum PeamanGender {
   female,
   other,
 }
+
+const ksPeamanGender = {
+  PeamanGender.unknown: 'unknown',
+  PeamanGender.male: 'male',
+  PeamanGender.female: 'female',
+  PeamanGender.other: 'other',
+};
